cf3a5bf059cc90d212202afcd0bba0f55c5bb61d
patrick <p.delong21@gmail.com>
2018-11-08 17:14:49 -0500

    nothing

--- Compress/src/Compress.java
+++ Compress/src/Compress.java
@@ -7,19 +7,19 @@ public class Compress {
         File toPath = new File(args[0].concat(".huff"));
 
         try{
-            //FileOutputStream foi = new FileOutputStream(toPath);
-            //ByteArrayOutputStream boi = new ByteArrayOutputStream(foi);
+
             byte [] buffer = Files.readAllBytes(file.toPath());
-            OutputStream oi = new FileOutputStream(toPath);
-            for (int i=0; i < buffer.length; i++){
-                oi.write(String.format("%8s", Integer.toBinaryString(buffer[i] & 0xFF)).replace(' ','0').getBytes());
-            }
-            oi.close();
+
         } catch(FileNotFoundException ex){
             System.out.println("Unable to open file: " + file);
         }
         catch (IOException ex){
             System.out.println("Error reading file + " + file);
         }
+    }
+
+    public int[] freq(Byte[] barray){
+        int[] f = new int[barray.length];
+        return f;
     }
 }
