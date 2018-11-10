cbf9c67d5c6f47d30969c7e10c116e6f632efaa2
patrick <p.delong21@gmail.com>
2018-11-09 20:47:46 -0500

    renamed Compress to henc

--- Compress/src/henc.java
+++ Compress/src/henc.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.util.PriorityQueue;
+import java.math.*;
 
 public class henc {
     public static void main(String[] args){
@@ -9,10 +11,11 @@ public class henc {
         try{
 
             byte [] buffer = Files.readAllBytes(file.toPath());
-            CollectFreq freq = new CollectFreq(buffer); // Build frequency table i.e two int arrays
+            CollectFreq freq = new CollectFreq(buffer); // Build frequency table i.e two unsorted int arrays
 
             for (int i = 0; i < freq.uchars.length; i++){
-                System.out.println("U = " + freq.uchars[i] + ", F = " + freq.fchars[i]);
+                System.out.println("U = " + Integer.toBinaryString(freq.uchars[i] & 0xFF)  + ", F = " + freq.fchars[i]);
+                //String.format("%8s", Integer.toBinaryString(freq.uchars[i] & 0xFF)).replace(' ','0')
             }
 
         } catch(FileNotFoundException ex){
@@ -23,4 +26,26 @@ public class henc {
         }
     }
 
+    public PriorityQueue Huffman(CollectFreq freq){
+        PriorityQueue Q = new PriorityQueue();
+        Q = BuildMinHeap(Q, freq);
+    }
+
+    public PriorityQueue BuildMinHeap(PriorityQueue Q, CollectFreq freq){
+        for (int i =(int)Math.floor(freq.fchars.length/2.0); i>=1; i--){
+            Q = MinHeapify(freq, i);
+        }
+        return Q;
+    }
+    public PriorityQueue MinHeapify(CollectFreq freq, int i){
+        int left = i - 1, right = i + 1;
+        int min = FindMin(i, left, right, freq);
+        if (min != i)
+    }
+    public int FindMin(int i, int j, int k, CollectFreq freq){
+        int min = freq.fchars[i];
+        if (min >= j) min = j;
+        if (min >= k) min = k;
+        return min;
+    }
 }
